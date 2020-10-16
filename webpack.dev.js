cabcc357ba9a16cf38d1cbfb481fe5ea115f6aa2
Lucas Seara Manoel <ls.manoel@gmail.com>
2020-10-16 15:05:13 -0300

    chore: add webpack prod and dev configs

---
+++ webpack.dev.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const common = require('./webpack.common')
+const { merge } = require('webpack-merge')
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
+  },
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template.dev.html'
+    })
+  ]
+})
